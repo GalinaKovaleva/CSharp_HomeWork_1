2e2353d6227a86e8147474ef7444d5bfe3e497d0
Galina <7334153@gmail.com>
2023-09-03 15:20:24 +0300

    feat: add tsks code_2

--- Program.cs
+++ Program.cs
@@ -14,3 +14,57 @@ int min = Math.Min(a, b);
 
 Console.WriteLine("Максимальное число: " + max);
 Console.WriteLine("Минимальное число: " + min);
+
+
+// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+ Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+int c = int.Parse(Console.ReadLine());
+
+int max = Math.Max(a, Math.Max(b, c));
+
+Console.WriteLine("Максимальное число: " + max);
+
+
+// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("Число является четным.");
+}
+
+else
+{
+    Console.WriteLine("Число не является четным.");
+}
+
+
+// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+ Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+
+Console.Write("Четные числа от 1 до " + N + ": ");
+
+for (int i = 2; i <= N; i += 2)
+{
+    Console.Write(i + " ");
+}
+Console.WriteLine();
